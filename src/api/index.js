712958b3102ade61e9b68420c556e66634caef20
Riole88 <boullemanitou@gmail.com>
2025-12-11 16:22:37 +0100

    Exo3

---
+++ src/api/index.js
@@ -0,0 +1,7 @@
+import API_KEY from './api.env'
+
+const baseURL = "https://https://morpion-api.edu.netlor.fr"
+const headers = {
+    'Content-type': 'application/json',
+    'Authorization': 'key=[${API_KEY}]'
+}
