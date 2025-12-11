8faa851287016c6e397750152bb120d108218a34
Riole88 <boullemanitou@gmail.com>
2025-12-11 17:40:29 +0100

    Modifiation de la baseURL Modify index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 import API_KEY from '../.env'
 
-const baseURL = "https://https://morpion-api.edu.netlor.fr"
+const baseURL = "https://morpion-api.edu.netlor.fr"
 const headers = {
     'Content-type': 'application/json',
     'Authorization': 'key=[${API_KEY}]'
 }
+export {baseURL,headers}
