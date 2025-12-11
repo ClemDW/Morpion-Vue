55e0ab9a6c92c8d1cf8cb017e196be6984b95c9c
Riole88 <boullemanitou@gmail.com>
2025-12-11 16:47:52 +0100

    Exo 4 : Add Home.js Modify index.js

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<script>
+import { RouterLink } from 'vue-router';
+
+
+</script>
+
+<template>
+    <div>
+        <nav>
+            <router-link to="/créer">Crée une partie</router-link>
+            <router-link to="/rejoindre">Rejoindre une partie</router-link>
+            <router-link to="/paramètre">Editer le profil</router-link>
+        </nav>
+    </div>
+</template>
