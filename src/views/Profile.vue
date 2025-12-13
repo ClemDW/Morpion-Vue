20e10a1e36a40b3d174ab4c6360171544e634736
Clément <clementdewasch54@gmail.com>
2025-12-13 13:47:36 +0100

    feat: exercice5 Profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,5 +1,5 @@
 <script>
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 import { baseURL, headers } from '@/api/index'
 import axios from 'axios'
 
@@ -13,7 +13,10 @@ export default {
             email: sessionStorage.getItem("user_email") || ""
         })
 
+        const errors = ref([])
+
         function updateUser() {
+            errors.value = []
             axios.put(baseURL+"/api/profile", {name: User.uname, email: User.email}, {headers: headers})
             .then((response) => {
                 console.log(response)
@@ -21,13 +24,24 @@ export default {
             })
             .catch((error) => {
                 console.log(error)
-                alert("Erreur lors de la mise à jour")
+                if(error.response && error.response.data) {
+                    if(error.response.data.errors) {
+                        errors.value = Object.values(error.response.data.errors).flat()
+                    } else if (error.response.data.message) {
+                        errors.value = [error.response.data.message]
+                    } else {
+                        errors.value = ["Une erreur est survenue lors de la mise à jour."]
+                    }
+                } else {
+                     errors.value = ["Impossible de contacter le serveur."]
+                }
             })
         }
 
         return {
             User,
             updateUser,
+            errors
         }
     },
 
@@ -55,8 +69,18 @@ export default {
 <template>
     <h1>Profil</h1>
     <h3>Modifier le nom</h3>
-    <input type="text" :value="User.uname">
+    <input type="text" v-model="User.uname">
     <h3>Modifier l'adresse mail</h3>
-    <input type="email" :value="User.email">
+    <input type="email" v-model="User.email">
     <button @click="updateUser">Modifier</button>
+    
+    <div v-if="errors.length" style="color: red; margin-top: 20px;">
+        <ul>
+            <li v-for="error in errors" :key="error">{{ error }}</li>
+        </ul>
+    </div>
 </template>
+
+<style scoped>
+        
+</style>
