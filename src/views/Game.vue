fab49ad8c80fa70f8d5248dcfe54feaf8bccd57b
Clément <clementdewasch54@gmail.com>
2025-12-14 22:32:34 +0100

    feat: Ex6 full -> Create empty game & URL redirect on active game

---
+++ src/views/Game.vue
@@ -0,0 +1,11 @@
+<script>
+    
+</script>
+
+<template>
+    <h2>Game room</h2>
+</template>
+
+<style scoped>
+        
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,29 @@
 <script>
 import { RouterLink } from 'vue-router';
+import axios from 'axios';
+import { baseURL, headers } from '@/api/index';
 
-
+export default {
+    methods: {
+        createGame() {
+            axios.post(baseURL + '/api/games', {}, { headers })
+                .then((response) => {
+                    console.log("Partie créée : ", response.data);
+                    this.$router.push({ name: 'game', params: { id: response.data.id } });
+                })
+                .catch((error) => {
+                    console.error("Erreur lors de la création de la partie", error);
+                    alert("Impossible de créer une partie.");
+                });
+        }
+    }
+}
 </script>
 
 <template>
     <div>
         <nav>
-            <router-link to="/create">Créer une partie</router-link>
-            <router-link to="/join">Rejoindre une partie</router-link>
+            <router-link to="/game" @click="createGame">Jouer</router-link>
             <router-link to="/profile">Editer le profil</router-link>
         </nav>
     </div>
